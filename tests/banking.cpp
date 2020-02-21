5d82b4a8af9950f2671df1ef78cf0de771bcae6f
FDVG <50839460+FDVG@users.noreply.github.com>
2020-02-21 13:44:59 +0300

    fix banking.cpp

--- tests/banking.cpp
+++ tests/banking.cpp
@@ -42,52 +42,53 @@ TEST(ACCOUUNT, All)
 
 TEST(TRANSACTION, All)
 {
-    Account ac1(100, 100);
-    Account ac2(100, 300);
-    Transaction t1;
-    t1.Make(ac2, ac1, 100);
+	public:
+		Account ac1(100, 100);
+		Account ac2(100, 300);
+		Transaction t1;
+		t1.Make(ac2, ac1, 100);
 
-    EXPECT_EQ(200, ac1.GetBalance());
-    EXPECT_EQ(200, ac2.GetBalance());
+		EXPECT_EQ(200, ac1.GetBalance());
+		EXPECT_EQ(200, ac2.GetBalance());
 
-    EXPECT_FALSE(t1.Make(ac2, ac1, 123));
+		EXPECT_FALSE(t1.Make(ac2, ac1, 123));
 
-    try
-    {
-        t1.Make(ac2, ac1, -10);
-    }
-    catch (const std::runtime_error &e)
-    {
-        std::string s1 = "sum can't be negative";
-        std::string s2 = e.what();
-        EXPECT_EQ(s1, s2);
-    }
-    try
-    {
-        t1.Make(ac2, ac1, 10);
-    }
-    catch (const std::runtime_error &e)
-    {
-        std::string s1 = "too small";
-        std::string s2 = e.what();
-        EXPECT_EQ(s1, s2);
-    }
-    try
-    {
-        t1.Make(ac2, ac2, 1011);
-    }
-    catch (const std::runtime_error &e)
-    {
-        std::string s1 = "invalid action";
-        std::string s2 = e.what();
-        EXPECT_EQ(s1, s2);
-    }
+		try
+		{
+			t1.Make(ac2, ac1, -10);
+		}
+		catch (const std::runtime_error &e)
+		{
+			std::string s1 = "sum can't be negative";
+			std::string s2 = e.what();
+			EXPECT_EQ(s1, s2);
+		}
+		try
+		{
+			t1.Make(ac2, ac1, 10);
+		}
+		catch (const std::runtime_error &e)
+		{
+			std::string s1 = "too small";
+			std::string s2 = e.what();
+			EXPECT_EQ(s1, s2);
+		}
+		try
+		{
+			t1.Make(ac2, ac2, 1011);
+		}
+		catch (const std::runtime_error &e)
+		{
+			std::string s1 = "invalid action";
+			std::string s2 = e.what();
+			EXPECT_EQ(s1, s2);
+		}
 
-    Transaction t2;
-    t2.Credit(ac2, 1000);
+		Transaction t2;
+		t2.Credit(ac2, 1000);
 
-    EXPECT_EQ(ac2.GetBalance(), 1000);
+		EXPECT_EQ(ac2.GetBalance(), 1000);
 
-    EXPECT_TRUE(t2.Debit(ac2, 800));
-    EXPECT_FALSE(t2.Debit(ac2, 800));
+		EXPECT_TRUE(t2.Debit(ac2, 800));
+		EXPECT_FALSE(t2.Debit(ac2, 800));
 }
